acea7c123687cdd89289260fdf6b996b4b7c2f60
porkio <porksb@163.com>
2020-11-04 11:50:44 +0800

    initial commit

---
+++ src/routes/users.js
@@ -0,0 +1,21 @@
+const router = require('koa-router')()
+
+router.prefix('/users')
+
+router.get('/', function (ctx, next) {
+  ctx.body = 'this is a users response!'
+})
+
+router.get('/bar', function (ctx, next) {
+  ctx.body = 'this is a users/bar response'
+})
+
+router.get('/profile/:username', (ctx, next) => {
+    const { username } = ctx.params
+    ctx.body = {
+        title: 'This is the userinfo of ' + username,
+        username
+    }
+})
+
+module.exports = router
