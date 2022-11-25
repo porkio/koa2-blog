3df91fc0c19f25ac9b8f9eb10c18a6b1b740ddcd
LSP <iolsp@qq.com>
2022-11-26 02:13:45 +0800

    fix: styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 const gulp = require('gulp')
 const autoprefixer = require('gulp-autoprefixer')
-const sass = require('gulp-sass')
+const sass = require('gulp-sass')(require('node-sass'))
 sass.compiler = require('node-sass')
 
 gulp.task('default', () => {
-    return gulp.src('./src/public/scss/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: 'last 2 version'
-        })).pipe(gulp.dest('./src/public/stylesheets'))
+	return gulp
+		.src('./src/public/scss/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(
+			autoprefixer({
+				browsers: 'last 2 version',
+			})
+		)
+		.pipe(gulp.dest('./src/public/stylesheets'))
 })
