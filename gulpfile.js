f980a81537b5b26ca8b0a04bb915ccc3248e9c63
LSP <iolsp@qq.com>
2022-11-26 04:28:39 +0800

    uglify js

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const autoprefixer = require('gulp-autoprefixer')
 const sass = require('gulp-sass')(require('node-sass'))
 sass.compiler = require('node-sass')
 const uglify = require('gulp-uglify-es').default
+const rename = require('gulp-rename')
 
 gulp.task('default', () => {
 	return gulp
@@ -19,6 +20,6 @@ gulp.task('default', () => {
 gulp.task('uglifyJs', () => {
 	return gulp
 		.src('./src/public/scripts/**/*.js')
-		.pipe(uglify())
+		.pipe(uglify()).pipe(rename({extname: '.min.js'}))
 		.pipe(gulp.dest('./src/public/js'))
 })
